0d454b973ffdf994170ad92441ccb88bd5d56a8e
jiwoo0204 <sally12234@naver.com>
2023-03-07 12:17:02 +0900

    init

---
+++ src/main/java/com/example/gccoffeeproject/GcCoffeeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gccoffeeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcCoffeeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcCoffeeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gccoffeeproject/GcCoffeeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gccoffeeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcCoffeeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
